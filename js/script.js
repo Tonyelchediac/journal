ca825de4c4a5f4b615ec2be2df9d784a29d668c1
Tonyelchediac <130777962+Tonyelchediac@users.noreply.github.com>
2025-07-01 22:01:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-      // Hamburger menu
-      const hamburger = document.getElementById('hamburger');
-      const navList = document.getElementById('nav-list');
+// Hamburger menu
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
-      hamburger.addEventListener('click', () => {
-        navList.classList.toggle('show');
-      });
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
 
-      // date
-      const date = new Date();
-      const options = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
-      document.querySelector('.to-day').textContent = date.toLocaleDateString('en-US', options);
+// date
+const date = new Date();
+const options = {
+  weekday: "long",
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+};
+document.querySelector(".to-day").textContent = date.toLocaleDateString(
+  "en-US",
+  options
+);
 
-
-
-
+// breaking news scrolling none
+const scrolling = document.querySelector('.home-right-side .breaking-news');
+if(window.innerWidth < 768){
+  scrolling.style.overflow = 'visible';
+  scrolling.style.maxHeight = '200vh';
+}
